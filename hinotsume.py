297b6620bc7efcd8c7e9248df55b15e8af149b6d
jendralhxr <jendral.hxr@gmail.com>
2020-07-10 15:46:52 +0900

    traffic counting from background subtracted image

---
+++ hinotsume.py
@@ -0,0 +1,76 @@
+# todo: single pixel horizontal shadow instead of block highlight
+
+#!/usr/bin/env python3
+import numpy as np
+import cv2
+import sys
+import math
+
+cap = cv2.VideoCapture(sys.argv[1])
+cap.set(cv2.CAP_PROP_POS_FRAMES, float(sys.argv[2]))
+frame = cap.read()
+
+threshold_min= 20
+
+crop_x_start= 0
+crop_x_stop= 1400
+crop_y_start= 0
+crop_y_stop= 90
+
+thickness_min= 10 # maximum width of bondo
+block_width= 60 # minimum width of vehicle
+
+framenum= 0
+while(1):
+	ret, frame = cap.read()
+	framenum+=1
+	image= frame[crop_y_start:crop_y_stop, crop_x_start:crop_x_stop]
+	
+	for i in range(crop_x_start, crop_x_stop-1):
+		for j in range(crop_y_stop-1, crop_y_start, -1):
+			if (image[j,i][0] or image[j,i][1] or image[j,i][2]) > threshold_min: # if detect object
+				for j in range(crop_y_start, crop_y_stop):
+					image[j,i][0] = 255
+				break
+				
+	# join to obtain contiguous block using green lines	
+	block_start= 0	
+	for i in range(crop_x_start, crop_x_stop-1):
+		if image[1,i][0] > threshold_min:
+			block_start= i
+		else:
+			if (i-block_start) < thickness_min: 
+				for j in range(crop_y_start, crop_y_stop):
+					image[j,i][1] = 255 # green
+			else: 
+				block_start= 0
+	
+	#remove spurious lines
+	block_start= 0
+	block_stop= 0
+	for i in range(crop_x_start, crop_x_stop-1):
+		if (image[1,i][0] == 255 or image[1,i][1] == 255) and block_start==0:
+			block_start= i
+		elif (image[1,i][0] == 0 and image[1,i][1] == 0) and block_stop==0:
+			block_stop= i
+			if ((block_stop-block_start) <= block_width) and block_start!=0 and block_stop != 0:
+				for n in range(block_start, block_stop):
+					for j in range(crop_y_start, crop_y_stop):
+						image[j,n][0] = 0 # blue
+						image[j,n][1] = 0 # green
+						image[j,n][2] = 0 # red
+			#print("{} start{} stop{}".format(framenum, block_start, block_stop))
+			block_start= 0
+			block_stop= 0
+					
+	# apply identifier
+	# detect passing from right or left
+	# infer the passes
+	
+	cv2.imshow('frame',image)
+	k = cv2.waitKey(1)	
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
