78f5c1debd16406874a54a5770e842324efb6954
jendralhxr <jendral.hxr@gmail.com>
2020-07-31 16:37:26 +0900

    interval between frames instead of maximum passing duration

--- hinotsume-filtercondense.m
+++ hinotsume-filtercondense.m
@@ -12,30 +12,31 @@ COL_POSITION    =7;
 raw=dlmread("logall.txt","\t, ");
 raw= sortrows(raw, [1 2]);
 
-for i=1:size(,1)
+for i=1:size(raw,1)
   raw(i, COL_WIDTH)= raw(i, COL_STOP) - raw(i, COL_START);
   raw(i, COL_POSITION)= (raw(i, COL_STOP) + raw(i, COL_START)) /2;
 endfor
 
-MAX_PASS= 2000.0;
+POS_JUMP= 20;
 
 clear traffic;
 start= 1;
 n=1;
 for i=2:size(raw,1)
-  if (raw(start,COL_ID) != raw(i,COL_ID)) || (raw(start,COL_DIRECTION) != raw(i,COL_DIRECTION)) ||  (( raw(i,COL_FRAMENUM) - raw(start,COL_FRAMENUM)) > MAX_PASS )
+  if (raw(start,COL_ID) != raw(i,COL_ID)) || (raw(start,COL_DIRECTION) != raw(i,COL_DIRECTION)) ||  (( abs (raw(i,COL_FRAMENUM) - raw(i-1,COL_FRAMENUM))) > POS_JUMP )
     stop= i-1;
     traffic(n,1)= raw(start, COL_ID);
     traffic(n,2)= raw(start, COL_FRAMENUM);
-    traffic(n,3)= raw(start, COL_DIRECTION);
-    traffic(n,4)= mean(raw(start:stop, COL_WIDTH));
-    traffic(n,5)= raw(stop, COL_FRAMENUM) - raw(start, COL_FRAMENUM);
+    traffic(n,3)= raw(stop, COL_FRAMENUM) - raw(start, COL_FRAMENUM);
+    traffic(n,4)= raw(start, COL_DIRECTION);
+    traffic(n,5)= mean(raw(start:stop, COL_WIDTH));
     traffic(n,6)= raw(start, COL_POSITION);
     traffic(n,7)= raw(stop, COL_POSITION);
     start=i;
   n+=1;
   endif
 endfor
+
 
 # sort from framenum/time of occurence
 traffic=sortrows(traffic, [2]); 
@@ -44,24 +45,22 @@ traffic=sortrows(traffic, [2]);
 do
   size_prev= size(traffic,1);    
   for n=1:size(traffic,1)-1
-    if traffic(n,3)==1 && traffic(n,7)<traffic(n,6)
+    if traffic(n,4)==1 && traffic(n,7)<traffic(n,6)
       traffic(n,:) = [];
     endif
-    if traffic(n,3)==0 && traffic(n,7)>traffic(n,6) 
+    if traffic(n,4)==0 && traffic(n,7)>traffic(n,6) 
       traffic(n,:) = [];
     endif
-    if traffic(n,5)<10
+    if traffic(n,3)<10
       traffic(n,:) = [];  
 	endif
   endfor
   size_cur= size(traffic,1);
 until (size_cur==size_prev)
-    
-traffic1=traffic;
 
+    
 #---------
 
-traffic=dlmread("ba.txt","\t");
 TIME_CLUSTER= 3 # mins
 
 traffic(:, COL_FRAMENUM) /= 3600; # now in minutes
